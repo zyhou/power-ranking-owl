1820e86139b1b76089ef46d836d281b2388e0228
zyhou <richard.maxime9@gmail.com>
2019-02-01 18:22:35 +0100

    try another

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.get('*', (req, res) => {
-    res.write('<h1><marquee direction=right>Hello from Express path `/` on Now 2.0!</marquee></h1>');
-    res.write('<h2>Go to <a href="/about">/about</a></h2>');
-    res.end();
+app.get('/tweet', (req, res) => {
+    res.send('Hello');
 });
 
 module.exports = app;
