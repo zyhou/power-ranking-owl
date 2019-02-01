3e0d0b80ab037f868bfcbd21b39511d64850332a
zyhou <richard.maxime9@gmail.com>
2019-02-01 17:58:10 +0100

    try exemple

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('*', (req, res) => {
+    res.write('<h1><marquee direction=right>Hello from Express path `/` on Now 2.0!</marquee></h1>');
+    res.write('<h2>Go to <a href="/about">/about</a></h2>');
+    res.end();
+});
 
-app.get('/tweet', (req, res) => res.send('Hello Twitter!'));
-
-app.listen(process.env.PORT || 3000);
+module.exports = app;
