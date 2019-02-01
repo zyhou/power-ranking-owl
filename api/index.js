16823000f9040117e07eae9a1de56fd73b165fe7
zyhou <richard.maxime9@gmail.com>
2019-02-01 18:26:29 +0100

    add url

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/tweet', (req, res) => {
-    res.send('Hello');
+    return res.send('/tweet');
+});
+
+app.get('/api/tweet', (req, res) => {
+    return res.send('/api/tweet');
 });
 
 module.exports = app;
