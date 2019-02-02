7a982b1cc723934cac74cc16794e6505a08531da
Maxime <richard.maxime9@gmail.com>
2019-02-02 01:29:55 +0100

    add twit lib

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const twit = require('twit');
+
 const app = express();
+
+const twitter = new twit({
+    consumer_key: '...',
+    consumer_secret: '...',
+    access_token: '...',
+    access_token_secret: '...',
+    timeout_ms: 60 * 1000,
+});
 
 app.get('/api/tweet', (req, res) => {
     return res.send('/api/tweet');
