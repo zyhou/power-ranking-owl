e7e573945e890fe4083c5d17233e17d49ffc4e97
zyhou <richard.maxime9@gmail.com>
2019-02-02 20:44:17 +0100

    try react_app env var

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    API_URL: process.env.API_URL || 'http://localhost:3001',
-    ROUTE_URL: process.env.ROUTE_URL || '/api/tweet',
+    API_URL: process.env.API_URL || process.env.REACT_APP_API_URL || 'totot',
+    ROUTE_URL: process.env.ROUTE_URL || process.env.REACT_APP_ROUTE_URL || '/api/tweet',
     CONSUMER_KEY: process.env.CONSUMER_KEY,
     CONSUMER_SECRET: process.env.CONSUMER_SECRET,
     ACCESS_TOKEN: process.env.ACCESS_TOKEN,
